5a4f33b5c09eb01847be9549cb43371eb69e0d4d
Chris Sidi <cubandressing@gmail.com>
2019-04-13 19:03:03 -0400

    Waiting for peripheral to power on
    Also:
    * Made peripheral a member variable to avoid it being disposed

--- myFirstPeripheral/myFirstPeripheral/ViewController.swift
+++ myFirstPeripheral/myFirstPeripheral/ViewController.swift
@@ -9,40 +9,54 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
     @IBOutlet weak var logView: UITextView!
+    
+    var peripheral: CBPeripheralManager? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let cofeValue = "Cafe\u{301} du 🌍".data(using: String.Encoding.utf8)
         
-        let service = CBMutableService(type: CBUUID(string: "CAFE"), primary: true)
+        peripheral = CBPeripheralManager(delegate: self, queue: nil)
         
-        service.characteristics = [
-            CBMutableCharacteristic(
-                type: CBUUID(string: "C0FE"),
-                properties: CBCharacteristicProperties.read,
-                value: cofeValue,
-                permissions: CBAttributePermissions.readable)
-        ]
-        
-        let manager = CBPeripheralManager()
-        
-        log("CB manager state: \(manager.state.rawValue)")
-        if (manager.state == CBManagerState.poweredOn) {
-            log("adding service")
-            manager.add(service)
-            
-            log("starting advertising")
-            manager.startAdvertising(nil)
+        log("BLE manager state: \(peripheral!.state.rawValue)")
+        if (peripheral!.state == CBManagerState.poweredOn) {
+            log("powered on already")
         }
         else {
-            log("not powered on. not doing much of anything.")
+            log("not powered on yet.")
         }
     }
     
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        log("new BLE manager state: \(peripheral.state.rawValue)")
+        if (peripheral.state == CBManagerState.poweredOn) {
+            log("powered on.")
+            
+            let cofeValue = "Cafe\u{301} du 🌍".data(using: String.Encoding.utf8)
+            
+            let service = CBMutableService(type: CBUUID(string: "CAFE"), primary: true)
+            
+            service.characteristics = [
+                CBMutableCharacteristic(
+                    type: CBUUID(string: "C0FE"),
+                    properties: CBCharacteristicProperties.read,
+                    value: cofeValue,
+                    permissions: CBAttributePermissions.readable)
+            ]
+
+            log("adding custom C0FE service")
+            peripheral.add(service)
+            
+            log("starting advertisement")
+            peripheral.startAdvertising(nil)
+            log("advertising")
+        }
+        else {
+            log("still not powered on.")
+        }
+    }
+
     func log(_ msg: String) {
         print(msg)
         
