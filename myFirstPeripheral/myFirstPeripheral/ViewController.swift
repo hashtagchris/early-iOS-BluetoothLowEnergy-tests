30330c9ab9260b95064b9fa6ff036eeeb264cb69
Chris Sidi <cubandressing@gmail.com>
2019-04-13 18:05:52 -0400

    Advertise a custom service with one characteristic

--- myFirstPeripheral/myFirstPeripheral/ViewController.swift
+++ myFirstPeripheral/myFirstPeripheral/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let cofeValue = "Cafe\u{301} du 🌍".data(using: String.Encoding.utf8)
+        
+        let service = CBMutableService(type: CBUUID(string: "CAFE"), primary: true)
+        
+        service.characteristics = [
+            CBMutableCharacteristic(
+                type: CBUUID(string: "C0FE"),
+                properties: CBCharacteristicProperties.read,
+                value: cofeValue,
+                permissions: CBAttributePermissions.readable)
+        ]
+        
+        let manager = CBPeripheralManager()
+        
+        log("CB manager state: \(manager.state)")
+        if (manager.state == CBManagerState.poweredOn) {
+            log("adding service")
+            manager.add(service)
+            
+            log("starting advertising")
+            manager.startAdvertising(nil)
+        }
+        else {
+            log("not powered on. not doing much of anything.")
+        }
     }
-
-
+    
+    func log(_ msg: String) {
+        print(msg)
+    }
 }
-
