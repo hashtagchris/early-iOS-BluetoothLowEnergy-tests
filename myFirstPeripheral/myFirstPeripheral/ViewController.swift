8174e1bfa2d9e21ae83b12d2a878075a7b0540e5
Chris Sidi <cubandressing@gmail.com>
2019-04-13 21:52:59 -0400

    Test message that's easier to inspect in bluetoothctl
    Translate CBManagerStates to strings.

--- myFirstPeripheral/myFirstPeripheral/ViewController.swift
+++ myFirstPeripheral/myFirstPeripheral/ViewController.swift
@@ -11,29 +11,20 @@ import CoreBluetooth
 
 class ViewController: UIViewController, CBPeripheralManagerDelegate {
     @IBOutlet weak var logView: UITextView!
-    
     var peripheral: CBPeripheralManager? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        log("initializing...")
         peripheral = CBPeripheralManager(delegate: self, queue: nil)
-        
-        log("BLE manager state: \(peripheral!.state.rawValue)")
-        if (peripheral!.state == CBManagerState.poweredOn) {
-            log("powered on already")
-        }
-        else {
-            log("not powered on yet.")
-        }
+        log("peripheral state: \(stateToString(peripheral!.state))")
     }
     
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        log("new BLE manager state: \(peripheral.state.rawValue)")
+        log("new peripheral state: \(stateToString(peripheral.state))")
         if (peripheral.state == CBManagerState.poweredOn) {
             log("powered on.")
-            
-            let cofeValue = "Cafe\u{301} du 🌍".data(using: String.Encoding.utf8)
             
             let service = CBMutableService(type: CBUUID(string: "CAFE"), primary: true)
             
@@ -41,7 +32,7 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
                 CBMutableCharacteristic(
                     type: CBUUID(string: "C0FE"),
                     properties: CBCharacteristicProperties.read,
-                    value: cofeValue,
+                    value: "*** Hello, would you like some coffee?".data(using: String.Encoding.utf8),
                     permissions: CBAttributePermissions.readable)
             ]
 
@@ -62,5 +53,24 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
         
         logView.text.append(msg)
         logView.text.append("\n")
+    }
+    
+    func stateToString(_ state: CBManagerState) -> String {
+        switch (state) {
+            case .unknown:
+                return "unknown"
+            case .resetting:
+                return "resetting"
+            case .unsupported:
+                return "unsupported"
+            case .unauthorized:
+                return "unauthorized"
+            case .poweredOff:
+                return "poweredOff"
+            case .poweredOn:
+                return "poweredOn"
+            @unknown default:
+                return "unknown unknowns"
+        }
     }
 }
