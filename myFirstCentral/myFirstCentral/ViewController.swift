70aa46d252d06c5bdaf0af783e1d1219227aeba8
Chris Sidi <cubandressing@gmail.com>
2019-04-14 10:09:37 -0400

    Add a central manager and logging

--- myFirstCentral/myFirstCentral/ViewController.swift
+++ myFirstCentral/myFirstCentral/ViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    
     @IBOutlet weak var logView: UITextView!
     @IBOutlet weak var c0feButton: UIButton!
     @IBOutlet weak var c0ffButton: UIButton!
+    var central: CBCentralManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        log("initializing...")
+        central = CBCentralManager(delegate: self, queue: nil)
+        log("central state: \(stateToString(central.state))")
     }
     
-    
-}
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        log("new central state: \(stateToString(central.state))")
+    }
 
+    func log(_ msg: String) {
+        print(msg)
+        
+        logView.text.append(msg)
+        logView.text.append("\n")
+    }
+    
+    func stateToString(_ state: CBManagerState) -> String {
+        switch (state) {
+        case .unknown:
+            return "unknown"
+        case .resetting:
+            return "resetting"
+        case .unsupported:
+            return "unsupported"
+        case .unauthorized:
+            return "unauthorized"
+        case .poweredOff:
+            return "poweredOff"
+        case .poweredOn:
+            return "poweredOn"
+        @unknown default:
+            return "unknown unknowns"
+        }
+    }
+}
