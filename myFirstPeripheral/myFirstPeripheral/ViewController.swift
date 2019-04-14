7e5bb08fab998958d4ebadd1e63804761c4438d2
Chris Sidi <cubandressing@gmail.com>
2019-04-13 23:23:07 -0400

    Add a second characteristic (C0FF)
    This one is readable with a dynamic value, rather than a cached value.

--- myFirstPeripheral/myFirstPeripheral/ViewController.swift
+++ myFirstPeripheral/myFirstPeripheral/ViewController.swift
@@ -12,6 +12,7 @@ import CoreBluetooth
 class ViewController: UIViewController, CBPeripheralManagerDelegate {
     @IBOutlet weak var logView: UITextView!
     var peripheral: CBPeripheralManager!
+    var requestCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +34,12 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
                     type: CBUUID(string: "C0FE"),
                     properties: CBCharacteristicProperties.read,
                     value: "*** Hello, would you like some coffee?".data(using: String.Encoding.utf8),
-                    permissions: CBAttributePermissions.readable)
+                    permissions: CBAttributePermissions.readable),
+                CBMutableCharacteristic(
+                    type: CBUUID(string: "C0FF"),
+                    properties: CBCharacteristicProperties.read,
+                    value: nil,
+                    permissions: CBAttributePermissions.readable),
             ]
 
             log("adding custom CAFE service")
@@ -46,6 +52,20 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate {
         else {
             log("still not powered on.")
         }
+    }
+
+    // This only gets called for the C0FF characteristic. The C0FE characteristic has a cached value.
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest)
+    {
+        log("Read request received")
+        
+        requestCount += 1
+        let value = "Read request #\(requestCount)"
+        log("response: \"\(value)\" (success)")
+        
+        request.value = value.data(using: String.Encoding.utf8)
+        
+        peripheral.respond(to: request, withResult: CBATTError.success)
     }
 
     func log(_ msg: String) {
